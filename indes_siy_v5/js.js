e31f2b61a82d4055d005277520fe04fb80e376c9
INDESBR <tecnologia@indes.org.br>
2021-12-20 15:25:05 -0300

    update

--- indes_siy_v5/js.js
+++ indes_siy_v5/js.js
@@ -7,22 +7,28 @@ for (i = 0; i < slb.length; i++) {
 }
 
 function clique(e) {
+
     let btn = e.target.id
 
-    for (let i = 0; i < slb.length; i++) {
-
-        vd[i].classList.remove('video-active')
+    for (i = 0; i < slb.length; i++) {
 
         switch (btn) {
+
             case "slb1":
+                vd[i].classList.remove('video-active')
                 vd[0].classList.add('video-active')
                 break
+
             case "slb2":
+                vd[i].classList.remove('video-active')
                 vd[1].classList.add('video-active')
                 break
+
             case "slb3":
+                vd[i].classList.remove('video-active')
                 vd[2].classList.add('video-active')
                 break
+
         }
     }
 }
